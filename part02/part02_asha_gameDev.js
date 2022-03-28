ad1d3f124f641d081004b3bad5e788d6389d8761
yoon-asha <yoon.asha97@gmail.com>
2022-03-28 17:46:02 +0900

    나이트 완료

---
+++ part02/part02_asha_gameDev.js
@@ -0,0 +1,19 @@
+function gameDev(N,M, location, direction, map) {
+
+}
+
+let output = gameDev(4,4,[1,1],0,[
+    [1,1,1,1],
+    [1,0,0,1],
+    [1,1,0,1],
+    [1,1,0,0]
+])
+
+
+// let test = outline_sea([
+//     [1,1,1,1],
+//     [1,0,0,1],
+//     [1,1,0,1],
+//     [1,1,0,1,]
+// ],4,4)
+console.log(output);
